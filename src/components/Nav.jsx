45f1db20d9471fda5d9a8fc6e69e0e3fb4a756ab
gerardnicolas <77805536+gerardnicolas@users.noreply.github.com>
2023-12-15 20:14:34 +0800

    commit

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,7 +6,7 @@ function Nav () {
             <p className='flex text-xs items-center font-semibold'>ged.dev</p>
             <div className='flex items-center'>
                 <a href="https://www.linkedin.com/in/gerard-nicolas-4287531a0/" target="_blank" rel='noreferrer' className='text-xs'> 
-                    <img src="./public/assets/linkedin-logo.webp" alt="LinkedIn Logo" className="w-6"/>
+                    <img src="/assets/linkedin-logo.webp" alt="LinkedIn Logo" className="w-6"/>
                 </a>
                 <a href="./src/assets/nicolas-resume.pdf" download="Resume">
                     <button className='flex text-[0.6rem] px-[0.25rem] rounded border-blue-600 bg-blue-600 decoration-blue hover:bg-blue-500 hover:border-blue-500'>
