26b5bf747415250c532aa93d7a58257061af18fa
gerardnicolas <77805536+gerardnicolas@users.noreply.github.com>
2023-12-15 20:41:32 +0800

    download button bug fix

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,7 +8,7 @@ function Nav () {
                 <a href="https://www.linkedin.com/in/gerard-nicolas-4287531a0/" target="_blank" rel='noreferrer' className='text-xs'> 
                     <img src="/assets/linkedin-logo.webp" alt="LinkedIn Logo" className="w-6"/>
                 </a>
-                <a href="./src/assets/nicolas-resume.pdf" download="Resume">
+                <a href="/assets/nicolas-resume.pdf" download="Resume">
                     <button className='flex text-[0.6rem] px-[0.25rem] rounded border-blue-600 bg-blue-600 decoration-blue hover:bg-blue-500 hover:border-blue-500'>
                         Resume
                     </button>
