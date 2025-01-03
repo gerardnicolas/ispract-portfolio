0418f4e9a914f96b77d7840b942d424a8d8bae74
gerardnicolas <77805536+gerardnicolas@users.noreply.github.com>
2025-01-03 12:02:57 +0800

    ui

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,20 +4,20 @@ const Hero = () => {
   const description = ``;
 
 return (
-    <div className="flex flex-col md:flex-row items-center justify-evenly p-10">
+    <div className="flex flex-col md:flex-row items-center justify-evenly p-10 gap-10">
         <div className="mb-5 md:mb-0">
             <img
                 src="/assets/barong-pic.JPG"
                 alt=""
-                className="w-48 h-48 md:w-96 md:h-96 object-cover object-[80%_20%] rounded-full"
+                className="w-48 h-48 md:w-96 md:h-96 object-cover object-[80%_20%] rounded-full shadow-lg"
                 style={{ minWidth: '12rem', minHeight: '12rem', maxWidth: '24rem', maxHeight: '24rem' }}
             />
         </div>
-        <div className="flex-col text-justify max-w-lg m-10">
+        <div className="flex-col text-justify max-w-lg max-h-min">
             <p className="text-xl md:text-2xl font-bold">Gerard Nicolas</p>
             <p className="text-base md:text-lg font-semibold">BS - Information Systems</p>
 
-            <p className="my-5 text-sm md:text-base">
+            <p className="my-5 md:text-base">
                 Hi there! I'm Gerard Nicolas, a recent graduate with a Bachelor’s
                 degree in Information Systems. I'm passionate about creating beautiful
                 and functional user interfaces. Currently, I'm focusing on becoming a
